1b1cd84215c22277dee59dfaea73052e91f1f25b
Abrhamtaye7 <Abrhamtayetessfaye@gmail.com>
2022-04-07 22:03:07 +0300

    new commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,14 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
-*main - entry point
-*Return: Always 0 (Success)
-*/
+ * main - Prints "with proper grammar, but the outcome is
+ *                a piece of art," followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-printf("\"with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
 }
