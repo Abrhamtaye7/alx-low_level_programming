8206255561e272edd8d04ac641ee17ea89a8903c
abrhamtaye7 <abrhamtayetessfaye@gmail.com>
2022-04-21 20:58:21 +0300

    main h edit

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,39 +6,15 @@
  */
 void print_number(int n)
 {
-	int x, y, z;
+	unsigned int x;
 
-	x = 0;
-	y = 1;
-	z = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		x = 1;
+		n *= 1;
 	}
-	while (z > 9 || z < -9)
-	{
-		y *= 10;
-		z /= 10;
-	}
-	while (y > 0)
-	{
-		if (y > 9)
-		{
-			if (!x)
-				_putchar((n / y % 10) + '0');
-			else
-				_putchar((n / y % 10) * -1 + '0');
-
-			y /= 10;
-		}
-		if (y == 1)
-		{
-			if (x)
-				_putchar((n % 10) * -1 + '0');
-			else
-				_putchar(n % 10 + '0');
-			y = 0;
-		}
-	}
+	x = n;
+	if (x / 10)
+		print_number(x / 10);
+	_putchar(x % 10 + '0');
 }
