d51a75966f475a1f19392b5c347b91f8c0d1c6e4
abrhamtaye7 <abrhamtayetessfaye@gmail.com>
2022-04-29 23:25:19 +0300

    fix 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - Entry ppoint
  * @argc: size of argv array
@@ -7,6 +8,7 @@
  */
 int main(int argc, char *argv[])
 {
+	(void) argc;
 	int x;
 
 	for (x = 0; x < argc; x++)
