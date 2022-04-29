765184fd802e2aeba1a2f5ae4fbd9ba9dbfe5ee1
abrhamtaye7 <abrhamtayetessfaye@gmail.com>
2022-04-29 22:16:17 +0300

    0x0A argc and argv commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: size of argv array
+ * @argv: array of size argc
+ * Return: Always 0 Sucess
+ */
+int main(int argc, char *argv[])
+{
+	int x:
+	for (x = 0; x, argc; x++)
+	{
+		printf("%s\n", argv [0];
+	}
+	return (0);
+} 
