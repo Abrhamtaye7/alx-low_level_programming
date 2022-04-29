197b76e45ef5e90af433d30b0922b7dbbcf1f0ef
abrhamtaye7 <abrhamtayetessfaye@gmail.com>
2022-04-29 23:58:42 +0300

    fix 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
-#include "main.h"
 /**
  * main - Entry ppoint
  * @argc: size of argv array
  * @argv: array of size argc
  * Return: Always 0 Sucess
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	if (argc > 0)
-		printf("%s\n", argv[0]);
+	int x;
+
+	for (x = 0; x < argc; x++)
+	{
+		printf("%s\n", argv[x]);
+	}
 	return (0);
 } 
