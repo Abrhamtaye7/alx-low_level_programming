4b2c72aa44d3689f0e83b8b3f9e28754d164e83f
abrhamtaye7 <abrhamtayetessfaye@gmail.com>
2022-05-09 16:03:20 +0300

    typedf commit one

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,7 @@
+## 0x0E-structures_typedef task
+	1. dog.h
+	2. 1-init dog.c
+	3. 2-print_dog.c
+	4. dog.h
+	5. 4-new_dog.c
+	6. 5-free_dog.c
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _main_h_
+#define _main_h_
+
+typedef  struct dog
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+int main(void)
+{
+	void init_dog(struct dog *d, char *name, float age, char *owner);
+	void print_dog(struct dog *d);
+	dog_t *new_dog(char *name, float age, char *owner);
+	void free_dog(dog_t *d);
+	#endif
+}
