ab37614f33d1a7b088a8704a7068cdb707bdb193
abrhamtaye7 <abrhamtayetessfaye@gmail.com>
2022-05-09 16:40:19 +0300

    new commit fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,13 +15,14 @@ typedef struct dog
 		char *name;
 			float age;
 				char *owner;
-} dog_t;
+}
+{
+    struct dog my_dog;
 
-int _strlen(char *s);
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-char *_strdup(char *str);
-
+    my_dog.name = "Poppy";
+    my_dog.age = 3.5;
+    my_dog.owner = "Bob";
+    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+    return (0);
+}
 #endif
