2d1c4b7a134d1b1acca848fd2dc72fce1e2f84b7
abrhamtaye7 <abrhamtayetessfaye@gmail.com>
2022-05-09 17:22:46 +0300

    fix dog two

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -5,25 +5,23 @@
 #include <stdlib.h>
 
 /**
- *  * struct dog - dog
- *   * @name: name of the dog
- *    * @age: how old the dog is
- *     * @owner: who owns the dog
- *      */
-typedef struct dog
+ * struct dog - Create a struct of a dog
+ * @name: Dog's name
+ * @age: Age of the dog
+ * @owner: Dog's owner
+ */
+struct dog
 {
-		char *name;
-		float age;
-		char *owner;
-} 
-typedef struct dog_t;
+	char *name;
+	float age;
+	char *owner;
+};
 
-int _strlen(char *s);
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strdup(char *str);
 
 #endif
-
