2617374b8af046c3a52a9c1a7559b39eef73fdd6
abrhamtaye7 <abrhamtayetessfaye@gmail.com>
2022-05-23 22:53:09 +0300

    fixing zero

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,10 +1,12 @@
 #include "lists.h"
+
 /**
   * print_listint - prints all elements of a listint_t list.
   * @h: pointer to first element in a list.
   *
-  * Return:  number of nodes.
+  * Return:  always count.
   */
+
 size_t print_listint(const listint_t *h)
 {
 	int count;
