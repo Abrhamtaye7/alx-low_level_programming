00c0b395d6770404f1a1318581901169bd92cf6c
abrhamtaye7 <abrhamtayetessfaye@gmail.com>
2022-05-27 01:08:09 +0300

    0x14 task bulk commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * converts a binary number to unsigned int
+ *
+ * return: converted number
+ */
+unsigned int binary_to_unit(const char *b);
+
+{
+	int i;
+	unsigned int dec_val = 0;
+	{
+	if (!b)
+		return (0);
+	for (i = 0; b[i]; i++)
+	}
+	return (dec_val);
+}
+
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+This readme file is for the task of 0x14-bit_manipulation
