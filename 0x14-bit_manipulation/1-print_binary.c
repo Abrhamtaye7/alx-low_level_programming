78bb45aafa398ae2edcec36d42c607b8a185d162
abrhamtaye7 <abrhamtayetessfaye@gmail.com>
2022-05-27 01:12:10 +0300

    correction 1,2 & 0

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,11 +1,10 @@
 #include "main.h"
-/**
- * print the binary number
- * 
- * return binary number
- */
 
-void print_binary(unsigned long int n);
+/**
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print in binary
+ */
+void print_binary(unsigned long int n)
 {
 	int i, count = 0;
 	unsigned long int current;
