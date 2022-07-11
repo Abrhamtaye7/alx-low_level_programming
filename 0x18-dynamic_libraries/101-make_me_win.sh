c6909c14e2aaa1e064987d86fee1fa6c7d3b5630
Abrhamtaye7 <abrhamtayetessfaye@gmail.com>
2022-07-11 22:18:54 +0300

    101

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. /tmp/ https://raw.githubusercontent.com/Abrhamtaye7/alx-low_level_programming/master/0x18-dynamic_libraries/libdynamic.so
+wget -P .. /tmp/ https://raw.githubusercontent.com/Abrhamtaye7/alx-low_level_programming/main/0x18-dynamic_libraries/libdynamic.so
 export LD_PRELOAD="/tmp/../libdynamic.so"
